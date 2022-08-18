9ccb39a1e39b4a6581174b63f0c2a3363febb7e7
Noah Hood <hood.noah@icloud.com>
2022-08-18 15:35:23 -0400

    bring closer to parity with formatting of main

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
---[[ init.lua ]] 
+-- [[ init.lua ]]
+
 -- LEADER
 -- These keybindings need to be defined before the first /
 -- is called; otherwise, it will default to "\"
@@ -6,24 +7,16 @@ vim.g.mapleader = ","
 vim.g.localleader = "\\"
 
 -- IMPORTS
-require('vars')      -- Variables
-require('opts')      -- Options
-require('keys')      -- Keymaps
-require('plug')      -- Plugins
+require('keys')     -- Keymaps
+require('vars')     -- Variables
+require('opts')     -- Options
+require('plug')     -- Plugins
 
--- SETUP
+-- PLUGINS
 require('nvim-tree').setup{}
 require('lualine').setup {
-  options = {
-    theme = 'dracula-nvim'
-  }
-}
-require('nvim-autopairs').setup{}
-require('nvim-treesitter.configs').setup{
-    ensure_installed = { "lua" },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-        enable = true
+    options = {
+        theme = 'dracula-nvim'
     }
 }
+require('nvim-autopairs').setup{}
--- lua/keys.lua
+++ lua/keys.lua
@@ -1,9 +1,5 @@
---[[ keys.lua ]]
+-- [[ keys.lua ]]
 local map = vim.api.nvim_set_keymap
-local buf_map = vim.api.nvim_buf_set_keymap
-
--- remap the key used to leave insert mode
--- map('i', 'jk', '', {})
 
 -- Toggle nvim-tree
 map('n', '<leader>n', [[:NvimTreeToggle]], {})
