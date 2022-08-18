15dfe733339be2190949f018c70169ec655170f5
Noah Hood <hood.noah@icloud.com>
2022-08-18 13:12:43 -0400

    implement working windows config

---
+++ init.lua
@@ -0,0 +1,28 @@
+--[[ init.lua ]] -- LEADER
+-- These keybindings need to be defined before the first /
+-- is called; otherwise, it will default to "\"
+vim.g.mapleader = ","
+vim.g.localleader = "\\"
+
+-- IMPORTS
+require('vars')      -- Variables
+require('opts')      -- Options
+require('plug')      -- Plugins
+require('keys')      -- Keymaps
+
+-- SETUP
+require('nvim-tree').setup{}
+require('lualine').setup {
+  options = {
+    theme = 'dracula-nvim'
+  }
+}
+require('nvim-autopairs').setup{}
+require('nvim-treesitter.configs').setup{
+    ensure_installed = { "lua" },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true
+    }
+}
