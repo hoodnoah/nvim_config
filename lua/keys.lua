69a52a8aba865cb6dd60e78858cb40f04ce40ed0
Noah Hood <hood.noah@icloud.com>
2022-08-18 09:38:30 -0400

    remove previous config options

--- lua/keys.lua
+++
@@ -1,9 +0,0 @@
--- [[ keys.lua ]]
-local map = vim.api.nvim_set_keymap
-
--- Toggle nvim-tree
-map('n', '<leader>n', [[:NvimTreeToggle]], {})
-
-map('n', '<leader>l', [[:IndentLinesToggle]], {})
-map('n', '<leader>t', [[:TagbarToggle]], {})
-map('n', '<leader>ff', [[:Telescope find_files]], {})
--- lua/vars.lua
+++
@@ -1,9 +0,0 @@
---[[ vars.lua ]]
-
-local g = vim.g         -- set g to vim.global
-g.t_co = 256            -- global.terminal_colorspace
-g.background = "dark"   -- vim background
-
--- Update the packpath
-local packer_path = vim.fn.stdpath('config') .. '/site'
-vim.o.packpath = vim.o.packpath .. ',' .. packer_path
