649db103b14ee30b20f47f8cad86fe3e9e5d11b6
Noah Hood <hood.noah@icloud.com>
2022-08-19 20:27:43 -0400

    remove all old config files

--- mac.sh
+++
@@ -1,7 +0,0 @@
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.config/nvim/site/pack/packer/start/packer.nvim
-
-rm -rf site/.git
-
-# Install fsharp autocomplete plugin
-dotnet tool install --global fsautocomplete
--- windows.ps1
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim\site\pack\packer\start\packer.nvim"
-
-rm -r -fo site/.git
-
-# Setup fsharp autocomplete
-dotnet tool install --global fsautocomplete
