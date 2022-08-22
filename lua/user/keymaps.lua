f7a77ede9716802ce56905a258d68832e75cc1ee
Noah Hood <hood.noah@icloud.com>
2022-08-22 10:46:45 -0400

    add toggleterm

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -19,11 +19,11 @@ vim.g.maplocalleader = " "
 --   command_mode = "c",
 
 -- Normal --
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+-- Better window navigation COMMENTED OUT AFTER INSTALLING TERM
+--[[ keymap("n", "<C-h>", "<C-w>h", opts) ]]
+--[[ keymap("n", "<C-j>", "<C-w>j", opts) ]]
+--[[ keymap("n", "<C-k>", "<C-w>k", opts) ]]
+--[[ keymap("n", "<C-l>", "<C-w>l", opts) ]]
 
 -- Resize with arrows
 keymap("n", "<SC-Up>", ":resize +2<CR>", opts)
