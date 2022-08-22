d364291806b1a4f4d71d11a6bcfd2c9e268c1943
Noah Hood <hood.noah@icloud.com>
2022-08-22 10:22:50 -0400

    limit treesitter languages to languages actually used

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,21 +1,24 @@
 local configs = require("nvim-treesitter.configs")
 configs.setup {
-  ensure_installed = "all",
-  sync_install = false, 
-  ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  rainbow = {
-    enable = true,
-    extended_mode = true,
-    max_file_lines = nil,
-  },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
+    ensure_installed = {"c_sharp", "cpp", "css", "dockerfile", "gitignore", "html", "javascript", "jsdoc", "json",
+                        "lua", "markdown", "python", "regex", "rust", "toml", "tsx", "typescript", "vue", "yaml"},
+    sync_install = false,
+    highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = {""}, -- list of language that will be disabled
+        additional_vim_regex_highlighting = true
+    },
+    indent = {
+        enable = true,
+        disable = {"yaml"}
+    },
+    rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil
+    },
+    context_commentstring = {
+        enable = true,
+        enable_autocmd = false
+    }
 }
